56eed610fa1fd7f8dc0c092834f3975f65e3fc8a
Rahul Jain <rahul.jain@cenx.com>
2018-05-07 14:32:16 -0400

    changed port to default 5000

--- templates/trigger.py
+++ templates/trigger.py
@@ -9,4 +9,4 @@ def post():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port={% if 'MASTER' in SERVICE_NAME|upper() %}10010{% else %}10011{% endif %})
+    app.run(host='0.0.0.0', port=5000)
